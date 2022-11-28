0e857d69720bc337a9838b9c17d3eb65bcaf3567
vigneshkalithas <vigneshkanna3333@gmail.com>
2022-11-28 17:54:21 +0530

    server integrated

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,4 +1,4 @@
 export const Config = {
-  // api : "https://server-cooker.vercel.app",
-  api: "http://localhost:4000",
+  api: "https://server-cooker.vercel.app",
+  // api: "http://localhost:4000",
 };
