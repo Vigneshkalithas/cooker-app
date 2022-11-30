99cf5be1522248bd24e378e347208a80adc98144
vigneshkalithas <vigneshkanna3333@gmail.com>
2022-11-30 10:28:15 +0530

    after api update

--- src/Config/Config.js
+++ src/Config/Config.js
@@ -1,4 +1,4 @@
 export const Config = {
-  // api: "https://server-cooker.vercel.app",
-  api: "http://localhost:4000",
+  api: "https://server-cooker-oijfes0rv-vigneshkalithas.vercel.app",
+  // api: "http://localhost:4000",
 };
